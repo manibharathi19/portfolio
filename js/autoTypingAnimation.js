a2344c9220f35fa5eecad5835745dae62645330f
Mani Bharathi <106468741+manibharathi19@users.noreply.github.com>
2024-06-10 11:11:24 -0400

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Mani Bharathi</span> I am a dedicated student at SKP Engineering College , currently honing my skills in various areas of technology. My expertise spans across WordPress, SQL, Python, AI, HTML, CSS, and JavaScript. I am passionate about web development and artificial intelligence, constantly seeking to expand my knowledge and improve my skill set. I thrive in dynamic environments and am eager to contribute to innovative projects that push the boundaries of technology. Currently, I am actively seeking an internship opportunity to apply my skills in a professional setting and gain valuable industry experience."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
